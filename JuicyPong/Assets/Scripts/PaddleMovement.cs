7531c4633c630bc8648ee1339d69e135f9119c78
Fib <pixelmonium@gmail.com>
2014-04-13 21:50:08 -0500

    Level layout & paddle movement

---
+++ JuicyPong/Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PlayerNum
+{
+	Player1,
+	Player2
+}
+
+public class PaddleMovement : MonoBehaviour {
+	
+	public float Speed = 5;
+	public float UpperBound;
+	public float LowerBound;
+	public PlayerNum PlayerSlot = PlayerNum.Player1;
+	private float vertDir;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (PlayerSlot == PlayerNum.Player1)
+		{
+			vertDir = Input.GetAxis("P1Vertical");
+		}
+		else
+		{
+			vertDir = Input.GetAxis("P2Vertical");
+		}
+
+		transform.position += Vector3.up * vertDir * Speed * Time.deltaTime;
+
+		if (transform.position.y > UpperBound || transform.position.y < LowerBound)
+			transform.position -= Vector3.up * vertDir * Speed * Time.deltaTime;
+	}
+}
