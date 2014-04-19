9d75f176de0e7035d22fc8b4e4c6aabe6e6e5623
Fib <pixelmonium@gmail.com>
2014-04-19 12:29:43 -0500

    Fixed ball movement & made the script simpler
    Added a physics material to make the ball 100% bouncy with no loss of
    energy. Changed the ball movement script to only give a initial force in
    the Initialize() function so the Update() and OnCollision2D() functions
    could be removed.

--- JuicyPong/Assets/Scripts/BallMovement.cs
+++ JuicyPong/Assets/Scripts/BallMovement.cs
@@ -1,49 +1,14 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class BallMovement : MonoBehaviour {
 
-	public float Speed = 5.0f;
-	public Vector3 dir;
+	public float InitialForce = 100.0f;
+	public Vector2 InitialDirection = Vector2.zero;
 
-	// Use this for initialization
+	// Initialization
 	void Start ()
 	{
-		dir = new Vector3(1, 1, 0);
-		dir.Normalize();
-		gameObject.rigidbody2D.AddForce(new Vector2(100 * Speed, 100 * Speed));
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		//transform.position += dir * Speed * Time.deltaTime;
-	}
-
-	void OnCollisionEnter2D(Collision2D collision)
-	{
-		//dir = Vector3.Reflect(dir, new Vector3(collision.contacts[0].normal.x, collision.contacts[0].normal.y, 0));
-		Vector3 n = new Vector3(collision.contacts[0].normal.x, collision.contacts[0].normal.y, 0);
-		Vector3 v = new Vector3(gameObject.rigidbody2D.velocity.x, gameObject.rigidbody2D.velocity.y, 0);
-
-		Vector3 reflectVec = Vector3.Reflect(v, n);
-
-		Vector2 inputVec = new Vector2(reflectVec.x, reflectVec.y);
-
-		gameObject.rigidbody2D.AddForce(collision.contacts[0].normal * Speed * 100);
-		//gameObject.rigidbody2D.AddForce(inputVec * Speed * 100);
-		Debug.Log("Ceiling collision");
-		//dir.Normalize();
-		//if (collision.gameObject.tag == Statics.CeilingTag)
-		//{
-			//Debug.Log("Ceiling collision");
-			//dir.y *= (-1);
-		//}
-
-		//if (collision.gameObject.tag == Statics.WallTag)
-		//{
-			//Debug.Log("Wall collision");
-			//dir.x = dir.x * (-1);
-		//}
+		InitialDirection.Normalize();
+		rigidbody2D.AddForce(InitialDirection * InitialForce);
 	}
 }
