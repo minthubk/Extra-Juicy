7b14d5f123bf6cc6dd02f5bc3970cc129a671c4a
Fib <pixelmonium@gmail.com>
2014-04-19 15:01:22 -0500

    Ball sounds
    The ball makes sounds now when it hits something.

---
+++ JuicyPong/Assets/Scripts/PlayHitSounds.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayHitSounds : MonoBehaviour {
+
+	public Transform CameraPosition;
+	public AudioClip[] HitSounds;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		audio.PlayOneShot(HitSounds[Random.Range(0, 3)]);
+	}
+}
